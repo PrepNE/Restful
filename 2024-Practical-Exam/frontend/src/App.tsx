fc0596b6dd16652ee2e10d8c0f068a60f9bad3ca
mikepn02 <nzaberamikepeter@gmail.com>
2025-04-20 19:58:20 +0200

    initial commit

---
+++ 2024-Practical-Exam/frontend/src/App.tsx
@@ -0,0 +1,90 @@
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes
+} from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import NotFound from "./pages/404";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import PublicRoute from "./routes/PublicRoute";
+import { AuthProvider } from "./hooks/useAuth";
+import useAuth from "./hooks/useAuth";
+import PageLoader from "./components/shared/PageLoader";
+import DashboardLayout from "./layouts/DashboardLayout";
+import Book from "./pages/dashboard/Book";
+import Overview from "./pages/dashboard/Overview";
+
+const Home = () => {
+  const { user } = useAuth()
+
+  return <h1 className="text-center text-8xl text-red-500 font-bold">Hello {user?.firstName + " " + user?.lastName}</h1>
+};
+
+function AppRoutes() {
+  const { user, initialLoading } = useAuth();
+
+  const isAuthenticated = !!user;
+
+  if (initialLoading) {
+    return <PageLoader />
+  }
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute isAuthenticated={isAuthenticated}>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <PublicRoute isAuthenticated={isAuthenticated}>
+            <Register />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <PublicRoute isAuthenticated={isAuthenticated}>
+            <Login />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute isAuthenticated={isAuthenticated}>
+            <DashboardLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<Overview />} />
+        <Route path="book" element={<Book />} />
+      </Route>
+
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+function App() {
+  return (
+    <RecoilRoot>
+      <Router>
+        <AuthProvider>
+          <AppRoutes />
+        </AuthProvider>
+      </Router>
+    </RecoilRoot>
+  );
+}
+
+export default App;
